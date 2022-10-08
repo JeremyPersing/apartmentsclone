ad1327f2ddf0c1a42881973ab78ad73b153a260a
JeremyPersing <jpersing1999@gmail.com>
2022-10-07 19:10:35 -0500

    standalone login with google

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -2,6 +2,7 @@ import { useNavigation } from "@react-navigation/native";
 import * as Facebook from "expo-auth-session/providers/facebook";
 import * as Google from "expo-auth-session/providers/google";
 import * as AppleAuthentication from "expo-apple-authentication";
+import { useEffect } from "react";
 
 import {
   appleLoginOrRegister,
@@ -15,17 +16,40 @@ import { useUser } from "./useUser";
 import { useLoading } from "./useLoading";
 
 export const useAuth = () => {
-  const [_, __, googlePromptAsync] = Google.useAuthRequest({
+  const [_, googleResponse, googleAuth] = Google.useAuthRequest({
     expoClientId:
       "1080382822276-eqklp58m1q9fl85m7aj89n1ofp8bdj7p.apps.googleusercontent.com",
-    iosClientId: "GOOGLE_GUID.apps.googleusercontent.com",
-    androidClientId: "GOOGLE_GUID.apps.googleusercontent.com",
+    iosClientId:
+      "1080382822276-a0ms51p5cfc523bivhchs8nk04u2scq0.apps.googleusercontent.com",
+    androidClientId:
+      "1080382822276-dqohv9donltabnijor1uun2765hstr4v.apps.googleusercontent.com",
     webClientId: "GOOGLE_GUID.apps.googleusercontent.com",
+    selectAccount: true,
   });
 
   const [___, ____, fbPromptAsync] = Facebook.useAuthRequest({
     clientId: "723313165600806",
   });
+
+  useEffect(() => {
+    async function loginUserWithGoogle(access_token: string) {
+      try {
+        setLoading(true);
+
+        const user = await googleLoginOrRegister(access_token);
+        handleSignInUser(user);
+      } catch (error) {
+        handleAuthError();
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (googleResponse?.type === "success") {
+      const { access_token } = googleResponse.params;
+      loginUserWithGoogle(access_token);
+    }
+  }, [googleResponse]);
 
   const { login } = useUser();
   const { goBack } = useNavigation();
@@ -84,24 +108,6 @@ export const useAuth = () => {
         const { access_token } = response.params;
 
         const user = await facebookLoginOrRegister(access_token);
-        handleSignInUser(user);
-      }
-    } catch (error) {
-      handleAuthError();
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const googleAuth = async () => {
-    try {
-      const response = await googlePromptAsync();
-      if (response.type === "success") {
-        setLoading(true);
-
-        const { access_token } = response.params;
-
-        const user = await googleLoginOrRegister(access_token);
         handleSignInUser(user);
       }
     } catch (error) {
